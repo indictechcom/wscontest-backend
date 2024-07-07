1c13be31e013c2097ccf72aa2565131eaed35fd7
RihaanBH-1810 <rihaan1810@gmail.com>
2024-07-07 16:03:28 +0530

    add: only run script on active contests

--- db_update.py
+++ db_update.py
@@ -8,53 +8,55 @@ def run():
     session = Session()
 
     # Fetch active contests from the database
-    contests = session.query(Contest).filter_by(status=True).all()
-
+    contests = session.query(Contest).all()
     user_agent = "IndicWikisourceContest/1.1 (Development; https://example.org/IndicWikisourceContest/;) pywikisource/0.0.5"
 
-
-
     for contest in contests:
+        # print(contest.name)
+        if dt.datetime.today() > contest.end_date:
+            contest.status = False            
+        if contest.status == False:
+            continue 
+        elif contest.status == True:
+            ws = WikiSourceApi(contest.lang, user_agent)
 
-        ws = WikiSourceApi(contest.lang, user_agent)
+            # Fetch contest books related to the contest
+            books = session.query(Book).filter_by(cid=contest.cid).all()
 
-        # Fetch contest books related to the contest
-        books = session.query(Book).filter_by(cid=contest.cid).all()
+            for book in books:
+                try:
+                    page_list = ws.createdPageList(book.name)
+                    # print(page_list)
 
-        for book in books:
-            try:
-                page_list = ws.createdPageList(book.name)
-                print(page_list)
+                    for page in page_list:
+                        ipage = IndexPage(book_name=book.name, page_name=page)
+                        response = ws.pageStatus(page)
+                        # print(response)
 
-                for page in page_list:
-                    ipage = IndexPage(book_name=book.name, page_name=page)
-                    response = ws.pageStatus(page)
-                    print(response)
+                        if response['proofread'] is not None:
+                            user = session.query(User).filter_by(user_name=response["proofread"]["user"], cid=contest.cid).first()
+                            if not user:
+                                user = User(user_name=response["proofread"]["user"], cid=contest.cid)
+                                session.add(user)
+                            ipage.proofreader_username = user.user_name
+                            proofread_time = parser.parse(response["proofread"]["timestamp"])
+                            ipage.proofread_time = proofread_time.strftime('%Y-%m-%d %H:%M:%S')
+                            ipage.p_revision_id = response["proofread"]["revid"]
 
-                    if response['proofread'] is not None:
-                        user = session.query(User).filter_by(user_name=response["proofread"]["user"], cid=contest.cid).first()
-                        if not user:
-                            user = User(user_name=response["proofread"]["user"], cid=contest.cid)
-                            session.add(user)
-                        ipage.proofreader_username = user.user_name
-                        proofread_time = parser.parse(response["proofread"]["timestamp"])
-                        ipage.proofread_time = proofread_time.strftime('%Y-%m-%d %H:%M:%S')
-                        ipage.p_revision_id = response["proofread"]["revid"]
+                        if response['validate'] is not None:
+                            user = session.query(User).filter_by(user_name=response["validate"]["user"], cid=contest.cid).first()
+                            if not user:
+                                user = User(user_name=response["validate"]["user"], cid=contest.cid)
+                                session.add(user)
+                            ipage.validator_username = user.user_name
+                            validate_time = parser.parse(response["validate"]["timestamp"])
+                            ipage.validate_time = validate_time.strftime('%Y-%m-%d %H:%M:%S')
+                            ipage.v_revision_id = response["validate"]["revid"]
 
-                    if response['validate'] is not None:
-                        user = session.query(User).filter_by(user_name=response["validate"]["user"], cid=contest.cid).first()
-                        if not user:
-                            user = User(user_name=response["validate"]["user"], cid=contest.cid)
-                            session.add(user)
-                        ipage.validator_username = user.user_name
-                        validate_time = parser.parse(response["validate"]["timestamp"])
-                        ipage.validate_time = validate_time.strftime('%Y-%m-%d %H:%M:%S')
-                        ipage.v_revision_id = response["validate"]["revid"]
+                        session.add(ipage)
 
-                    session.add(ipage)
-
-            except Exception as e:
-                print(f"Error in {contest.name} contest: {e}")
+                except Exception as e:
+                    print(f"Error in {contest.name} contest: {e}")
 
         
     session.commit()
