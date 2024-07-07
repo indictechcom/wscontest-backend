cc0d5b33684f742ccfe43caa04ac13e299428d71
RihaanBH-1810 <rihaan1810@gmail.com>
2024-07-07 15:42:41 +0530

    add: db update script for the updating the stats

--- db_update.py
+++ db_update.py
@@ -2,8 +2,8 @@ from pywikisource import WikiSourceApi
 import json
 import datetime as dt
 from pytz import timezone
-from models import Contest, Book, IndexPage, Session
-
+from models import Contest, Book, IndexPage, Session, User
+from dateutil import parser
 def run():
     session = Session()
 
@@ -12,10 +12,10 @@ def run():
 
     user_agent = "IndicWikisourceContest/1.1 (Development; https://example.org/IndicWikisourceContest/;) pywikisource/0.0.5"
 
-    all_contests = {}  # Dictionary to hold all contest results
+
 
     for contest in contests:
-        con = {}
+
         ws = WikiSourceApi(contest.lang, user_agent)
 
         # Fetch contest books related to the contest
@@ -23,20 +23,41 @@ def run():
 
         for book in books:
             try:
-                con[book.name] = {}
-                page_list = ws.createdPageList(book.name.split(':')[1])
+                page_list = ws.createdPageList(book.name)
+                print(page_list)
 
                 for page in page_list:
-                    con[book.name][page] = {}
-                    con[book.name][page] = ws.pageStatus(page)
-            except Exception as e: 
+                    ipage = IndexPage(book_name=book.name, page_name=page)
+                    response = ws.pageStatus(page)
+                    print(response)
+
+                    if response['proofread'] is not None:
+                        user = session.query(User).filter_by(user_name=response["proofread"]["user"], cid=contest.cid).first()
+                        if not user:
+                            user = User(user_name=response["proofread"]["user"], cid=contest.cid)
+                            session.add(user)
+                        ipage.proofreader_username = user.user_name
+                        proofread_time = parser.parse(response["proofread"]["timestamp"])
+                        ipage.proofread_time = proofread_time.strftime('%Y-%m-%d %H:%M:%S')
+                        ipage.p_revision_id = response["proofread"]["revid"]
+
+                    if response['validate'] is not None:
+                        user = session.query(User).filter_by(user_name=response["validate"]["user"], cid=contest.cid).first()
+                        if not user:
+                            user = User(user_name=response["validate"]["user"], cid=contest.cid)
+                            session.add(user)
+                        ipage.validator_username = user.user_name
+                        validate_time = parser.parse(response["validate"]["timestamp"])
+                        ipage.validate_time = validate_time.strftime('%Y-%m-%d %H:%M:%S')
+                        ipage.v_revision_id = response["validate"]["revid"]
+
+                    session.add(ipage)
+
+            except Exception as e:
                 print(f"Error in {contest.name} contest: {e}")
 
-        con["LastUpdate"] = dt.datetime.now(timezone('UTC')).strftime("%A, %d. %B %Y %I:%M%p") + ' UTC'
         
-        all_contests[contest.name] = con  # Add contest results to the dictionary
-        
-    print(json.dumps(all_contests, indent=4))  # Print all contest results as a formatted JSON string
+    session.commit()
 
     # Close the session
     session.close()
